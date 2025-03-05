f18d68407943278a3b54d2dce36a6d7f32c8281b
Sourav Bhowal <souravbhowal15@gmail.com>
2025-03-06 00:41:14 +0530

    fixed?

--- apps/frontend/src/app/not-found.tsx
+++ apps/frontend/src/app/not-found.tsx
@@ -1,7 +1,7 @@
 
 export default function NotFound() {
   return (
-    <div className="text-white">
+    <div className="text-white flex items-center justify-center h-screen">
         <h1>404 - Page Not Found</h1>
     </div>
   )
