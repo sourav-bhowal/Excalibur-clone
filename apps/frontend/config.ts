eaaa2d17bf64c4c679da92ebbf76c4b8f1500b98
Sourav Bhowal <souravbhowal15@gmail.com>
2025-03-06 21:48:34 +0530

    maybe?

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  HTTP_BACKEND_URL: process.env.HTTP_BACKEND_URL || "http://excalibur_backend.souravbhowal.site",
-  WS_BACKEND_URL: process.env.WS_BACKEND_URL || "ws://excalibur_ws.souravbhowal.site",
+  HTTP_BACKEND_URL: process.env.HTTP_BACKEND_URL || "http://excalibur-backend.souravbhowal.site",
+  WS_BACKEND_URL: process.env.WS_BACKEND_URL || "ws://excalibur-ws.souravbhowal.site",
   AUTH_SECRET: process.env.AUTH_SECRET || "secret",
 };
--- packages/envs/index.ts
+++ packages/envs/index.ts
@@ -9,8 +9,8 @@ conf({
 const _config = {
   JWT_SECRET: process.env.JWT_SECRET || "secret",
   RABBITMQ_URL: process.env.RABBITMQ_URL,
-  HTTP_BACKEND_URL: process.env.HTTP_BACKEND_URL || "http://excalibur_backend.souravbhowal.site",
-  WS_BACKEND_URL: process.env.WS_BACKEND_URL || "ws://excalibur_ws.souravbhowal.site",
+  HTTP_BACKEND_URL: process.env.HTTP_BACKEND_URL || "http://excalibur-backend.souravbhowal.site",
+  WS_BACKEND_URL: process.env.WS_BACKEND_URL || "ws://excalibur-ws.souravbhowal.site",
   HTTP_PORT: process.env.HTTP_PORT || 3002,
   WS_PORT: process.env.WS_PORT || 3003,
 };
